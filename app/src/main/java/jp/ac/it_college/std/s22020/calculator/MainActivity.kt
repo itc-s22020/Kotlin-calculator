f1543995e131a0ae3c3683679c74c2c22a49c37b
itc-s22020 <s22020@std.it-college.ac.jp>
2023-07-13 21:33:22 +0900

    [create]calculator key layout 電卓キー配列のボタン作成

---
+++ app/src/main/java/jp/ac/it_college/std/s22020/calculator/MainActivity.kt
@@ -0,0 +1,13 @@
+package jp.ac.it_college.std.s22020.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
