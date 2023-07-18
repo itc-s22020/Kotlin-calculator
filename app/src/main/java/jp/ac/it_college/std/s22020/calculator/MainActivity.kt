ab5da755b78e5e3f1dd7f1f7156be221c9b98cdb
itc-s22020 <s22020@std.it-college.ac.jp>
2023-07-18 21:42:01 +0900

    [add+change] textView add + status bar color change. TextViewの表示 ステータスバーの色を変更

--- app/src/main/java/jp/ac/it_college/std/s22020/calculator/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s22020/calculator/MainActivity.kt
@@ -1,13 +1,34 @@
 package jp.ac.it_college.std.s22020.calculator
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+var sText = ""
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.statusBarColor = Color.LTGRAY
+
+
+        findViewById<Button>(R.id.btnOne).setOnClickListener(this)
+
     }
+
+
+    override fun onClick(view: View) {
+        var textView = findViewById<TextView>(R.id.textView)
+        when (view.id) {
+            R.id.btnOne -> {
+                sText += "1"
+                textView.text = sText
+            }
+        }
+    }
+
 }
