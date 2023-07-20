342ffa43ca7e3621a047d5c325367dec099d35a6
itc-s22020 <s22020@std.it-college.ac.jp>
2023-07-20 15:45:03 +0900

    [add]equal button implemented some functions. イコールの機能を一部実装  +四則演算の計算機能  +括弧の計算順序  +計算式の例外処理

--- app/src/main/java/jp/ac/it_college/std/s22020/calculator/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s22020/calculator/MainActivity.kt
@@ -1,11 +1,13 @@
 package jp.ac.it_college.std.s22020.calculator
 
+import android.annotation.SuppressLint
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 var sText = ""
 
@@ -21,11 +23,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.btnBC, R.id.btnAC,
             R.id.btnPlus, R.id.btnMinus, R.id.btnTimes, R.id.btnDivided,
             R.id.btnDot, R.id.btnPercent,
-            R.id.btnBrackets
+            R.id.btnBrackets, R.id.btnEqual
         )
         for (v in btnList) findViewById<Button>(v).setOnClickListener(this)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onClick(view: View) {
         val textView = findViewById<TextView>(R.id.textView)
         when (view.id) {
@@ -58,8 +61,115 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     else { "(" }
             }
 
+            R.id.btnEqual -> {
+                sText = try {
+                    Parser(sText).expr().toString()
+                } catch (e: NumberFormatException) {
+                    "Error"
+                }
+            }
+
         }
+        if (sText.dropLast(1) == "Error") {
+            sText = sText.last().toString()
+            textView.text = sText
+        } else {
         textView.text = sText
+        }
     }
 
 }
+
+internal open class Source(private val str: String) {
+    private var pos = 0
+    fun peek(): Int {
+        return if (pos < str.length) {
+            str[pos].code
+        } else -1
+    }
+
+    operator fun next() {
+        ++pos
+    }
+}
+
+internal class Parser(str: String) : Source(str) {
+    private fun number(): Int {
+        val sb = StringBuilder()
+        var ch: Int
+        while (peek().also { ch = it } >= 0 && Character.isDigit(ch)) {
+            sb.append(ch.toChar())
+            next()
+        }
+        return sb.toString().toInt()
+    }
+
+    // expr = term, {("+", term) | ("-", term)}
+    fun expr(): Int {
+        var x = term()
+        while (true) {
+            when (peek()) {
+                '+'.code -> {
+                    next()
+                    x += term()
+                    continue
+                }
+
+                '-'.code -> {
+                    next()
+                    x -= term()
+                    continue
+                }
+            }
+            break
+        }
+        return x
+    }
+
+    // term = factor, {("*", factor) | ("/", factor)}
+    private fun term(): Int {
+        var x = factor()
+        while (true) {
+            when (peek()) {
+                '×'.code -> {
+                    next()
+                    x *= factor()
+                    continue
+                }
+
+                '÷'.code -> {
+                    next()
+                    x /= factor()
+                    continue
+                }
+            }
+            break
+        }
+        return x
+    }
+
+    // factor = factor = [spaces], ("(", expr, ")") | number, [spaces]
+    private fun factor(): Int {
+        val ret: Int
+        spaces()
+        if (peek() == '('.code) {
+            next()
+            ret = expr()
+            if (peek() == ')'.code) {
+                next()
+            }
+        } else {
+            ret = number()
+        }
+        spaces()
+        return ret
+    }
+
+    private fun spaces() {
+        while (peek() == ' '.code) {
+            next()
+        }
+    }
+}
+
+
